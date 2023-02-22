ecc168884617df9717105becfa93533febd2196d
Rodney Asante <rodneykasante@gmail.com>
2023-02-22 13:11:56 -0800

    Fix betty in shell_loop.c

--- shell_loop.c
+++ shell_loop.c
@@ -44,14 +44,15 @@ int hsh(info_t *info, char **av)
 }
 
 /**
- * find_builtin - finds a builtin command
- * @info: the parameter & return info struct
- *
- * Return: -1 if builtin not found,
- * 	0 if builtin executed successfully,
- * 	1 if builtin found but not successful,
- * 	2 if builtin signals exit()
- */
+* find_builtin - finds a builtin command
+* @info: the parameter & return info struct
+*
+* Return: -1 if builtin not found,
+* 0 if builtin executed successfully,
+* 1 if builtin found but not successful,
+* 2 if builtin signals exit()
+*/
+
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
